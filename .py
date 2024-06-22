d09497a31dbf5301964cf4c47a9c57a14b27c6d1
anderson feix <anderson.feix@hotmail.com>
2024-06-22 14:17:32 -0300

    meu primeiro commit

---
+++ .py
@@ -0,0 +1,22 @@
+print("Jornada dos dados")
+
+
+# variáveis
+idade = 24
+nome = "anderson"
+
+# print
+
+print(idade)
+
+
+# soma
+
+nova_idade = idade +2
+print(nova_idade)
+
+# lista 
+lista = [1,2,3,4,5]
+
+# lista invertida
+lista_invertida = lista.reverse()
